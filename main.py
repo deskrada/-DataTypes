ef02bd0446da03c84c2d6fd95ad0710d722b60c6
deskrada <92560412+deskrada@users.noreply.github.com>
2022-09-05 03:14:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Fundamental DataTypes
+#int and float
+
+#int = enteros
+#float = decimales
+#type = tipo de data type
+
+print(type(2 + 4))
+print(2 -4)
+print(2 * 4)
+print(type(2 / 4))
+
+print(2 ** 4)
+print(2 // 4)
+
